7b21ff69d0642e1942de97d6503e23db30ac1524
sanmu <hdy9515@dingtalk.com>
2021-02-18 16:53:05 +0800

    FEAT: 动态组件HelloWorld

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,30 +2,39 @@
   <div class="home">
      {{ $store.state.storage.accessToken }}
     <img alt="Vue logo" src="../assets/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <component :is="dynHelloWorld"></component>
+
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-import { onMounted, getCurrentInstance } from 'vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import { onMounted, getCurrentInstance, defineAsyncComponent } from 'vue'
 
 export default {
   name: 'Home',
   // https://v3.vuejs.org/guide/composition-api-setup.html#setup
   setup(props, context) {
+    const dynHelloWorld = defineAsyncComponent(() => import('@/components/HelloWorld.vue'))
+
     onMounted(() => {
       const { proxy } = getCurrentInstance()
       console.log('mounted 1')
     })
+
+    return {
+      dynHelloWorld
+    }
   },
   mounted() {
     console.log('mounted 2')
     this.$store.commit('storage/setAccessToken', 'token')
   },
-  components: {
-    HelloWorld
-  }
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
